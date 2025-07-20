961baa3cecd50f73d03b8860b5921db997fdf806
Juan Enriquez <juan03192001@gmail.com>
2025-07-20 16:09:56 -0700

    finished practice project rock, paper, scissors

--- Rock Paper Sissors/script.js
+++ Rock Paper Sissors/script.js
@@ -1,16 +1,42 @@
 // const mainCont = document.querySelector("#game-container"); 
-let rock = 3;
-let paper = 2;
-let scissors = 1;
-
-let playerScore = 0;
-let computerScore = 0;
+let playerChoice = "";
 
 console.log("hello world");
 
-function playGame(){
-    let choice = Math.floor(Math.random() * 3);
-    if (choice === 1){
+function play(playerChoice){
+    let rand = Math.floor(Math.random() * 3 + 1); // gives a random number in between 1 and 3
+    let compSelection = "";
+    let result = "";
 
+    if (rand === 1){
+        compSelection = "rock";
     }
-}
+    else if(rand === 2){
+        compSelection = "paper"
+    }
+    else if(rand === 3){
+        compSelection = "scissor";
+    }
+
+    result = win(playerChoice, compSelection);
+    console.log(`Player Choice: ${playerChoice}`);
+    console.log(`Computer Choice: ${compSelection}`);
+    alert(result);
+
+};
+
+function win(player, computer){
+    if(player === computer){
+        return "tie";
+    }
+
+    if ((player === "rock" && computer === "scissor") || 
+        (player === "scissor" && computer === "paper") || 
+        (player === "paper" && computer === "rock")){
+        
+        return "win";
+    }
+    else{
+        return "lose";
+    }
+};
