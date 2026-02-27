214cd1683a274da2d78b63ba6afdb6494e68ca51
Juan Enriquez <juan03192001@gmail.com>
2026-02-26 20:10:58 -0800

    Added to practice folders

--- JavaScript Exercises/DSA/Arrays/Filter_Map_Reduce_practice.js
+++ JavaScript Exercises/DSA/Arrays/Filter_Map_Reduce_practice.js
@@ -1,23 +1,28 @@
 const people = [
-    {'name': 'jay', 'last': 'jung', 'age': 30},
-    {'name': 'juin', 'last': 'wang', 'age': 25},
-    {'name': 'yhu', 'last': 'ung', 'age': 36},
-    {'name': 'ujun', 'last': 'ang', 'age': 55},
-    {'name': 'naruto', 'last': 'geto', 'age': 38},
-    {'name': 'Yugi', 'last': 'huz', 'age': 18}
+    {name: 'jay', last: 'jung', age: 30},
+    {name: 'juin', last: 'wang', age: 25},
+    {name: 'yhu', last: 'ung', age: 36},
+    {name: 'ujun', last: 'ang', age: 55},
+    {name: 'naruto', last: 'geto', age: 38},
+    {name: 'Yugi', last: 'huz', age: 18}
 ];
 
 
 // Some practice with .filter
-console.log(people.filter(a => a.age < 30));
+console.log(people.filter(a => aage< 30));
 
 
 // .map
-
+console.log(people.map(a => ))
 
 // .reduce
 
 
 // .sort
 console.log(`This is the people object sorted from least
-    to greatest: ${JSON.stringify(people.sort((a, b) => a.age - b.age), null, 2)}`);
+    to greatest: ${JSON.stringify(people.sort((a, b) => a.age- b.age), null, 2)}`);
+
+
+// .then
+
+// .every
