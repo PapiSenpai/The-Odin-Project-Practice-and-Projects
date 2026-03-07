0390934e309a90191b0dd73637797b5afaf48eaa
Juan Enriquez <63441250+PapiSenpai@users.noreply.github.com>
2026-03-06 21:40:21 -0800

    a

---
+++ Svelte/Svelte_Site_Practice/src/view/MapView/MapView.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Map from 'ol/Map.js';
+    import OSM from 'ol/source/OSM.js';
+    import TileLayer from 'ol/layer/Tile.js';
+    import View from 'ol/View.js';
+
+    const map = new Map({
+        target: 'map',
+        layers: [
+            new TileLayer({
+                source: new OSM(),
+            }),
+        ],
+        view: new View({
+            center: [0, 0],
+            zoom: 2,
+        }),
+    });
+</script>
+
+<div class="map-component">
+
+    <div class="map-viewer">
+
+    </div>
+</div>
+
+<style>
+
+</style>
