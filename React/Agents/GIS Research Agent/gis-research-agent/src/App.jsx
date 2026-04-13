2ef42742363bce15686506029636069959e04b75
Juan Enriquez <63441250+PapiSenpai@users.noreply.github.com>
2026-04-13 14:06:39 -0700

    aProbably delete later
    Probably delete later

--- React/Agents/GIS Research Agent/gis-research-agent/src/App.jsx
+++ React/Agents/GIS Research Agent/gis-research-agent/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Products from './components/productTable'
 import './App.css'
 
 function App() {
@@ -29,7 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Products />
     </>
   )
 }
