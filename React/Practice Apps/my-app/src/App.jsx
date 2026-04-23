43ccb73ab6d269d5f258df6a4a8a12e26cddba88
Juan Enriquez <juan03192001@gmail.com>
2026-04-23 06:22:16 -0700

    Practice with GSAP

--- React/Practice Apps/my-app/src/App.jsx
+++ React/Practice Apps/my-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { GsapTo } from './pages/GsapTo'
 import { GsapForm } from './pages/GsapFrom'
+import { GsapFromTo } from './pages/GsapFromTo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,8 @@ function App() {
       <GsapTo></GsapTo>
       <br />
       <GsapForm></GsapForm>
+      <br />
+      <GsapFromTo></GsapFromTo>
     </>
   )
 }
--- React/Practice Apps/my-app/src/pages/GsapFromTo.tsx
+++ React/Practice Apps/my-app/src/pages/GsapFromTo.tsx
@@ -6,12 +6,18 @@ export const GsapFromTo = () => {
     // Each time you want to use gsap, you must use the hook
     useGSAP(() => {
         // This is one of the fundamental methods in GSAP
-        //  accepts an ID and applies props to element that is targeted (in this case #blue-box)
-
-        gsap.fromTo('#blue-box', {
+        //  accepts TWO IDs/Classes and applies props to element that is targeted (in this case #blue-box)
+        // You can define both states here
+        gsap.fromTo('#green-box', {
+            x:0,
+            rotation: 90,
+            borderRadius: '0%'
+        },
+        {
             x:250, // the animates across the x-axis
             repeat: -1, // sets if user wants the repeat animations
             yoyo: true, // loop animation
+            borderRadius: 360,
             rotation: 360,
             duration: 1,
             ease: 'bounce.in'
@@ -22,7 +28,15 @@ export const GsapFromTo = () => {
     return(
         <>
             <h1>GsapFromTo</h1>
-
+            <p>
+                The <code>gsap.fromTo()</code> method is used to animate elements from a new state to a new state.
+                <br /> The <code>gsap.from()</code> and <code>gsap.to()</code> methods, but the difference is that
+                the <code>gsap.fromTo()</code> method animates elements from a new state to a new state, while the
+                <code>gsap.from()</code> method animates elements from a new state to their current states and the
+                <code>gsap.to()</code> method animates elements from their current state to a new state.
+            </p>
+            <p>
+            </p>
 
             <div id="green-box" className="w-20 h-20 bg-green-200 border-lg"></div>
         </>
